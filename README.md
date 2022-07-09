af3845c3c1c521ff542d06592f3ab6de58f4d8fa
Xiao Zhuowei <xiaozhuowei@mail.iggcas.ac.cn>
2022-07-09 11:31:09 +0800

    0709_update

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 
 DiTingTools aims to provide modules for developing, training, evaluating, and applying deep-learning-based methods to solve data processing tasks in seismology. By combining large datasets such as DiTing, STEAD, INSTANCE, and so on, we hope to build solid benchmarks for various seismological studies so that deep-learning-enabled data processing methods can be better evaluated and improved. And eventually, they could become more practical and powerful in seismological research. In that sense, DiTingTools is similar to an excellent previous work named 'Seisbench' (Woollam et al., 2022; https://github.com/seisbench/seisbench). Yet, in technique aspects, DiTingTools uses TensorFlow while Seisbench uses PyTorch. Besides, their code structures are quite different as well. We would like DiTingTools to be a beneficial supplement to the existing works and contribute to the development of deep-learning-enabled seismology.
 
-谛听工具包主要为开发、训练、测评和应用基于深度学习的天然地震数据处理方法提供帮助。我们希望通过综合多个大型地震数据集，为多个地震学数据处理任务提供基准测试，以更好改进这些基于深度学习的处理方法，使它们更加实用和有效，更好的为地震学研究服务。在这一点上，DiTingTools与此前优秀的Seisbench (Woollam 等人., 2022; https://github.com/seisbench/seisbench )相似。DiTingTools基于TensorFlow而SeisBench基于PyTorch,此外二者的代码结构设计也完全不同。我们希望谛听工具包可以作为已有工作的有益补充，并更好帮助人工智能算法在地震学研究中落地应用。
+谛听工具包主要为开发、训练、测评和应用基于深度学习的天然地震数据处理方法提供帮助。我们希望通过综合多个大型地震数据集，为多个地震学数据处理任务提供基准测试，以更好改进这些基于深度学习的处理方法，使它们更加实用和有效，更好的为地震学研究服务。在这一点上，DiTingTools与此前优秀的工具包Seisbench (Woollam 等人., 2022; https://github.com/seisbench/seisbench )相似。DiTingTools基于TensorFlow而SeisBench基于PyTorch,此外二者的代码结构设计也完全不同。我们希望谛听工具包可以作为已有工作的有益补充，并更好帮助人工智能算法在地震学研究中落地应用。
 
 ## Getting Started [教程]
 
@@ -54,9 +54,9 @@ This module is undergoing rapid development. The demo version is for presentatio
 
 该模块仍在开发阶段。目前原型版本为谛听训练营和科大“地震学算法与程序培训班”使用。
 
-The cleaning and update on the demo version will be finished before 12th Jul. 2022.
+The cleaning and update on the demo version will be finished before 15th Jul. 2022.
 
-完整原型版代码整理将在7月12号之前上传完成。
+完整原型版代码整理将在7月15号之前上传完成。
 
 The continuous development of DiTingTools is going to be a challenging long-term task. We welcome any suggestions or feedback.
 
---
+++ dtt/dev/azi/train.py
@@ -0,0 +1,70 @@
+from src.tfDataset.BAZDataset import get_Azi_training_dataset 
+from src.models.DiTingBAZ_Basic import DiTingBAZ_Basic
+
+import keras
+from keras.callbacks import ModelCheckpoint, LearningRateScheduler, ReduceLROnPlateau, EarlyStopping
+import tensorflow as tf
+import os
+import yaml
+import argparse
+import pickle
+import matplotlib.pyplot as plt
+import numpy as np
+
+def train(cfgs=None):
+    if cfgs==None:
+        print('Empty Config Error')
+        return
+
+    if cfgs['Training']['Model']['type'] == 'DiTingBAZ_Basic':
+        model = DiTingBAZ_Basic(cfgs)
+    else:
+        print('Model Type Error!')
+        return
+    
+    print('Done Creating Model')
+    if cfgs['Training']['use_pre_trained_model']:
+        model.load_weights(cfgs['Training']['PreTrainedPath'])
+        print('Done Load weights')
+    
+    training_dataset, validation_data_gen = get_Azi_training_dataset(cfgs)
+    
+    print('Done Creating Dataset')
+
+    TASK_ID = cfgs['Training']['TASK_ID']
+    if os.path.exists(cfgs['Training']['filepath']):
+        pass
+    else:
+        os.makedirs(cfgs['Training']['filepath'])
+    filepath = cfgs['Training']['filepath'] + TASK_ID + '.weights.{epoch:02d}-{val_loss:.2f}.hdf5'
+    checkpoint = ModelCheckpoint(filepath, monitor='val_loss', save_best_only=True, mode='auto', period=1)
+    print('Done Creating Generator')
+    
+    earlyStoping = EarlyStopping(monitor='val_loss', patience=10)
+    lr_reducer = ReduceLROnPlateau(factor=0.5,cooldown=0,patience=5,min_lr = cfgs['Training']['min_lr'])
+    
+    hist = model.fit(training_dataset.repeat().batch(cfgs['Training']['batchsize']).prefetch(tf.data.experimental.AUTOTUNE),
+                        workers=cfgs['Training']['Workers'],
+                        use_multiprocessing=cfgs['Training']['multiprocessing'],
+                        callbacks=[checkpoint,lr_reducer,earlyStoping],
+                        epochs=cfgs['Training']['epochs'],
+                        steps_per_epoch=cfgs['Training']['steps_per_epoch'],
+                        validation_data=validation_data_gen.repeat().batch(cfgs['Training']['batchsize']).prefetch(tf.data.experimental.AUTOTUNE),
+                        validation_steps=cfgs['Training']['validation_steps']
+                        )
+
+    histpath = cfgs['Training']['histpath']  + TASK_ID + '_hist.hdf5'
+    with open(histpath, 'wb') as file_pi:
+        pickle.dump(hist.history, file_pi)
+    model.save(cfgs['Training']['histpath'] + TASK_ID + '_last.hdf5')
+
+    return
+    
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='DiTingTools: Back-azimuth regression Training')
+    parser.add_argument('--config-file', dest='config_file', type=str, help='Path to Configuration file')
+    args = parser.parse_args()
+    cfgs = yaml.load(open(args.config_file), Loader=yaml.SafeLoader)
+    os.environ['CUDA_VISIBLE_DEVICES'] = cfgs['Training']['GPU_ID']
+    train(cfgs)
